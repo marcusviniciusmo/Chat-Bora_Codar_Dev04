cfd484dc781aaa20c7eec790b6d798c00e6eed15
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2023-03-09 19:10:21 +0000

    SearchUser component styling

--- frontend/src/components/SearchUser/index.tsx
+++ frontend/src/components/SearchUser/index.tsx
@@ -1,12 +1,14 @@
 import { SearchUserProps } from "../../types/SearchUser";
+import { Container } from "./styles";
 
 export function SearchUser(props: SearchUserProps) {
   return (
-    <>
+    <Container>
       <input
         type='text'
+        placeholder='Search user or message'
         onChange={props.setInput()}
       />
-    </>
+    </Container>
   );
 };
