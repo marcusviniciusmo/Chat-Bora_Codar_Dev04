1d549d5f2fbf15852b8a69fcd9549a0b877572f8
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2023-03-23 19:11:30 +0000

    Historic component creation

--- frontend/src/components/Content/index.tsx
+++ frontend/src/components/Content/index.tsx
@@ -1,4 +1,5 @@
 import { ContactInfo } from "../ContactInfo";
+import { Historic } from "../Historic";
 
 export function Content() {
   return (
@@ -8,19 +9,7 @@ export function Content() {
       <ContactInfo />
       <h3>AQUI VAI UM BOTAO - Close</h3>
 
-      <h6>Hoje 11:30</h6>
-
-      <h4>Cecilia - 11:30</h4>
-      <h5>Tive uma ideia incrível para um projeto!😍</h5>
-
-      <h4>Você - 11:32</h4>
-      <h5>Sério? Me conta mais.</h5>
-
-      <h4>Cecilia - 11:34</h4>
-      <h5>E se a gente fizesse um chat moderno e responsivo em apenas uma semana?</h5>
-
-      <h4>Você - 11:36</h4>
-      <h5 >#boraCodar!🚀</h5>
+      <Historic />
 
       <h6>Digite sua mensagem</h6>
       <h3>AQUI VAI UM BOTAO - ENVIAR</h3>
---
+++ frontend/src/components/Historic/index.tsx
@@ -0,0 +1,21 @@
+export function Historic() {
+  return (
+    <>
+      <h1>HISTORIC Component</h1>
+
+      <h6>Hoje 11:30</h6>
+
+      <h4>Cecilia - 11:30</h4>
+      <h5>Tive uma ideia incrível para um projeto!😍</h5>
+
+      <h4>Você - 11:32</h4>
+      <h5>Sério? Me conta mais.</h5>
+
+      <h4>Cecilia - 11:34</h4>
+      <h5>E se a gente fizesse um chat moderno e responsivo em apenas uma semana?</h5>
+
+      <h4>Você - 11:36</h4>
+      <h5 >#boraCodar!🚀</h5>
+    </>
+  );
+};
