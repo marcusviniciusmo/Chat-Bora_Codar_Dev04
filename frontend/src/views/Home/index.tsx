4588049956df9d286c184e08e21172d03438494e
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2023-03-23 17:17:13 +0000

    Chat component creation

--- frontend/src/views/Home/index.tsx
+++ frontend/src/views/Home/index.tsx
@@ -1,29 +1,11 @@
+import { Chat } from "../../components/Chat";
+
 export function Home() {
   return (
     <>
       <h1>HOME View</h1>
 
-      <h3>AQUI VAI UMA FOTO</h3>
-      <h4>Cecilia Sassaki</h4>
-      <h5>Online</h5>
-      <h3>AQUI VAI UM BOTAO - Close</h3>
-
-      <h6>Hoje 11:30</h6>
-
-      <h4>Cecilia - 11:30</h4>
-      <h5>Tive uma ideia incrível para um projeto!😍</h5>
-
-      <h4>Você - 11:32</h4>
-      <h5>Sério? Me conta mais.</h5>
-
-      <h4>Cecilia - 11:34</h4>
-      <h5>E se a gente fizesse um chat moderno e responsivo em apenas uma semana?</h5>
-
-      <h4>Você - 11:36</h4>
-      <h5 >#boraCodar!🚀</h5>
-
-      <h6>Digite sua mensagem</h6>
-      <h3>AQUI VAI UM BOTAO - ENVIAR</h3>
+      <Chat />
     </>
   );
 };
