52a0717f9bee73353b04bc08b8f05676dc745848
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2023-03-23 19:06:38 +0000

    ContactInfo component creation

---
+++ frontend/src/components/ContactInfo/index.tsx
@@ -0,0 +1,11 @@
+export function ContactInfo() {
+  return (
+    <>
+      <h1>CONTACT INFO Component</h1>
+
+      <h3>AQUI VAI UMA FOTO</h3>
+      <h4>Cecilia Sassaki</h4>
+      <h5>Online</h5>
+    </>
+  );
+};
--- frontend/src/components/Content/index.tsx
+++ frontend/src/components/Content/index.tsx
@@ -1,11 +1,11 @@
+import { ContactInfo } from "../ContactInfo";
+
 export function Content() {
   return (
     <>
       <h1>CONTENT Component</h1>
 
-      <h3>AQUI VAI UMA FOTO</h3>
-      <h4>Cecilia Sassaki</h4>
-      <h5>Online</h5>
+      <ContactInfo />
       <h3>AQUI VAI UM BOTAO - Close</h3>
 
       <h6>Hoje 11:30</h6>
