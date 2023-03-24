6732ebe5c0770391b1bdd78d8957c9b0e70c518a
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2023-03-24 06:53:57 +0000

    UsersList component creation

--- frontend/src/components/Chat/index.tsx
+++ frontend/src/components/Chat/index.tsx
@@ -1,9 +1,12 @@
+import { UsersList } from "../UsersList";
 import { Content } from "../Content";
 
 export function Chat() {
   return (
     <>
       <h1>CHAT Component</h1>
+
+      <UsersList />
 
       <Content />
     </>
---
+++ frontend/src/components/UsersList/index.tsx
@@ -0,0 +1,5 @@
+export function UsersList() {
+  return (
+    <h1>USERS LIST Component</h1>
+  );
+};
