4206f09fbaf378996d06ed79092293e79e82c363
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2023-03-28 08:54:31 +0100

    Chat component styling

--- frontend/src/components/Chat/index.tsx
+++ frontend/src/components/Chat/index.tsx
@@ -1,14 +1,15 @@
 import { UsersList } from "../UsersList";
 import { Content } from "../Content";
+import { Container, UserArea } from "./styles";
 
 export function Chat() {
   return (
-    <>
-      <h1>CHAT Component</h1>
-
-      <UsersList />
+    <Container>
+      <UserArea>
+        <UsersList />
+      </UserArea>
 
       <Content />
-    </>
+    </Container>
   );
 };
---
+++ frontend/src/components/Chat/styles.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+`;
+
+export const UserArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
