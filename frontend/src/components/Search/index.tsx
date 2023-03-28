0b2a7c470d961ccc48053091f647febea8daeaa7
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2023-03-28 17:14:37 +0100

    Search component filtering contact list

--- frontend/src/components/Search/index.tsx
+++ frontend/src/components/Search/index.tsx
@@ -1,5 +1,18 @@
-export function Search() {
+type SearchProps = {
+  inputSearch: string;
+  setInput: Function;
+};
+
+export function Search(props: SearchProps) {
   return (
-    <h1>SEARCH Component</h1>
+    <>
+      <h1>SEARCH Component</h1>
+
+      <input
+        type='text'
+        value={props.inputSearch}
+        onChange={e => props.setInput(e.target.value)}
+      />
+    </>
   );
 };
--- frontend/src/components/UsersList/index.tsx
+++ frontend/src/components/UsersList/index.tsx
@@ -1,11 +1,42 @@
+import { useState } from "react";
 import { Search } from "../Search";
 
 export function UsersList() {
+  const [inputSearch, setInputSearch] = useState<string>('');
+
+  const contacts = [
+    {
+      id: 1,
+      name: 'Cecilia'
+    }, {
+      id: 2,
+      name: 'Cecilia 2'
+    }, {
+      id: 3,
+      name: 'Cecilia - Antigo'
+    }, {
+      id: 4,
+      name: 'Amor'
+    }
+  ];
+
+  const filteredList = contacts.filter((item) => {
+    return item.name.toLowerCase().includes(inputSearch.toLowerCase())
+  });
+
   return (
     <>
       <h1>USERS LIST Component</h1>
 
-      <Search />
+      <Search inputSearch={inputSearch} setInput={setInputSearch} />
+
+      {
+        filteredList.map((contact) => {
+          return (
+            <span key={contact.id}>{contact.name}</span>
+          )
+        })
+      }
     </>
   );
 };
