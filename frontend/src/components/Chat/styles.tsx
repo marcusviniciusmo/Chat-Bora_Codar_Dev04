d976fa7ad33bd09988f3a9a3ca5306e2623491ad
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2023-06-28 15:13:45 +0100

    Added border on Chat component

--- frontend/src/components/Chat/styles.tsx
+++ frontend/src/components/Chat/styles.tsx
@@ -7,8 +7,9 @@ export const Container = styled.div`
 `;
 
 export const UserArea = styled.div`
-  padding: 3.2rem 0rem;
+  padding: 3.2rem 3.2rem 3.2rem 1rem;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+  border-right: 0.1rem solid var(--border-color);
 `;
